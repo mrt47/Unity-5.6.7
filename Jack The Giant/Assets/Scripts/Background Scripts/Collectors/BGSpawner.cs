088e914dfd68840c052bc93262835c9bc6f7a943
Murat Kuran <muratkuran85@gmail.com>
2019-04-20 01:17:05 +0300

    Background Collector scripts and objects are added

---
+++ Jack The Giant/Assets/Scripts/Background Scripts/Collectors/BGSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGSpawner : MonoBehaviour {
+
+	private GameObject[] backgrounds ;
+
+	private float lastYPosition;
+
+	void Awake(){
+		GetBackgroundsAndSetYPosition ();
+	}
+
+	void GetBackgroundsAndSetYPosition(){
+		backgrounds = GameObject.FindGameObjectsWithTag ("Background");
+
+		lastYPosition = backgrounds [0].transform.position.y;
+
+		for (int i = 1; i < backgrounds.Length; i++){
+			if (lastYPosition > backgrounds[i].transform.position.y){
+				lastYPosition = backgrounds [i].transform.position.y;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "Background"){
+			if (target.gameObject.transform.position.y == lastYPosition){
+				Vector3 temp = target.gameObject.transform.position;
+				float height = ((BoxCollider2D)target).size.y;
+
+				for (int i = 0; i < backgrounds.Length; i++){
+					if (!backgrounds[i].activeInHierarchy){
+						temp.y -= height;
+						lastYPosition = temp.y;
+						backgrounds [i].transform.position = temp;
+						backgrounds [i].gameObject.SetActive (true);
+						break;
+					}
+				}
+				Debug.Log ("test");
+			}
+		}
+	}
+}
